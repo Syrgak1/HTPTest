83e759ab1669fd3263a30690d2b5764560c8ad54
Sergio Tollini <syrgakkenzhebaev@gmail.com>
2023-12-21 16:04:37 +0600

    Выполнил правки

--- src/main/java/exercise1/HomeWork1.java
+++ src/main/java/exercise1/HomeWork1.java
@@ -5,6 +5,6 @@ public class HomeWork1 {
         task1();
     }
     private static void task1(){
-        System.out.println("Решение первой задачи");
+        System.out.println("Решение первой задачи!");
     }
 }
